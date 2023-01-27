33895ce4771f1b02c4fbd75a88fb6866b90d6878
Mikolaj <spiningscythe@gmail.com>
2023-01-27 18:45:03 +0100

    hotterfix

--- launcher.sh
+++ launcher.sh
@@ -23,11 +23,7 @@ xset dpms force off
 echo "Launcher: screen off"
 
 # Shedule next run.
-minutes=$(echo $((12 + $RANDOM % 2)))
-hours=$(echo $((50 + $RANDOM % 21)))
-hold=(($minutes * $hours))
+hold=$(echo $((43200 + $RANDOM % 3600)))
+systemd-run --on-active=$hold ./launcher.sh
 
-at now + "$hold" minutes -f ./launcher.sh
-
-next=$(atq | awk '{print $5}')
-echo "Launcher: next run scheduled at $next"
+echo "Launcher: next run will start in $hold seconds"
